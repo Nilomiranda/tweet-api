3ed62fa917fa4c256a797814f56125220d93283a
Nilomiranda <nilomiranda3@gmail.com>
2021-03-23 21:37:52 -0300

    Created tests, services and helpers to deal with authorization, and token decoding

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -2,11 +2,24 @@ class JsonWebToken
   # secret to encode and decode the token
   HMAC_SECRET = Rails.application.secrets.secret_key_base
 
-  def self.encode(payload, expiration = 24.hours_from_now)
+  def self.encode(payload, expiration = 24.hours.from_now)
     # set token's expiration to 24 hours from token creation time
-    payload[:ext] = expiration.to_i
+    payload[:exp] = expiration.to_i
 
     # sign token with application secret
     JWT.encode(payload, HMAC_SECRET)
+  end
+
+  def self.decode(token)
+    puts("TOKEN", token)
+    # get payload: first element in decoded Array
+    body = JWT.decode(token, HMAC_SECRET)[0]
+    HashWithIndifferentAccess.new body
+
+    # rescue all decode errors
+
+  rescue JWT::DecodeError => e
+    puts("DECODE ERROR", e)
+    raise ExceptionHandler::InvalidToken, e.message
   end
 end
